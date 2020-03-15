98edb0616f9799918f9ab057c5f9e14f28253e06
Timothy Sharkey <Techne3@gmail.com>
2020-03-15 17:01:18 -0600

    added income and expense functions to filter through the amounts and add or subtract depending on if its expense/income

--- expense/src/components/IncomeExpenses.js
+++ expense/src/components/IncomeExpenses.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 function IncomeExpenses() {
+  const { transactions } = useContext(GlobalContext);
+
+  //
+  const amounts = transactions.map(trans => trans.amount);
+
+  // once we get the amounts we filter through anything > 0
+  // then use reduce to add them all together
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  // once we get the amounts we filter through anything < 0
+  // then use reduce to subtract them all together
+  const expense =
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -(1).toFixed(2);
+
   return (
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
-        <p className="money plus">+$0.00</p>
+        <p className="money plus">${income}</p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p className="money minus">-$0.00</p>
+        <p className="money minus">${expense}</p>
       </div>
     </div>
   );
