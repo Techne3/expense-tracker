32e289978f7853105ad55030e5d2296e355591e3
Timothy Sharkey <Techne3@gmail.com>
2020-03-15 15:58:31 -0600

    created structure for blance and header components added styling/

---
+++ expense/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import Balance from "./components/Balance";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div className="containier">
+        <Balance />
+      </div>
+    </div>
+  );
+}
+
+export default App;
