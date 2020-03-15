0e1ff2e36ffa40622d8a17a1c509f54a8848bac9
Timothy Sharkey <Techne3@gmail.com>
2020-03-15 16:51:24 -0600

    Set the balance component to show dynamic totals from the amounts

--- expense/src/components/Balance.js
+++ expense/src/components/Balance.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 function Balance() {
+  const { transactions } = useContext(GlobalContext);
+
+  // Map through the trans to get the amounts
+  const amount = transactions.map(transactions => transactions.amount);
+  // total adds all the amounts together and set it to to decimal places
+  const total = amount.reduce((acc, item) => (acc += item), 0).toFixed(2);
   return (
     <>
       <h4>Your Balance</h4>
-      <h1>$0.00</h1>
+      <h1>${total}</h1>
     </>
   );
 }
